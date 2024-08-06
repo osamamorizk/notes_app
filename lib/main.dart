4e3cfbeedd74fc68d45fac81935f942756d6b2ee
osamamorizk <osamarizk262@gmail.com>
2024-08-06 18:03:59 +0300

    change them of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_page.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -9,6 +10,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: NotesPage(),
+    );
   }
 }
